714274ffbb25ec1aeb540f28271ca677fcfda0a4
Vasavik-ua <47318025+Vasavik-ua@users.noreply.github.com>
2023-09-17 13:08:50 +0100

    Add check is digit

--- Hw.15.py
+++ Hw.15.py
@@ -1,12 +1,35 @@
-def exit_quest(value):
+def exit_quest(value):  # Check input value for exit.
     check_list = ["вихід", "exit", "quit", "e", "q"]
     for item in range(len(check_list)):
         if check_list[item] == (value.lower()):
             return True
-        else:
-            pass
     return False
 
-a = 'e'
-b = exit_quest(a)
-print(b)
+
+def check_isdigit(value):
+    check_minus = value.find('-')
+    if check_minus > 0:
+        value.replace('-', '', 1)
+    check_point = value.find('.')
+    check_comma = value.find(',')
+    if check_point > 0 or check_comma > 0:
+        if check_point > 0:
+            value.replace('.', '', 1)
+        elif check_comma > 0:
+            value.replace(',', '', 1)
+    if value.isdigit():
+        return True
+    else:
+        print(f'Ви ввели неправильне число: {value}')
+        return False
+
+
+
+
+
+
+while True:
+    input_value = input('Number required: ')
+    if exit_quest(input_value):
+        break
+
