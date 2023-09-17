53a9622426574b1b41b17e77ff5639c328516338
Vasavik-ua <47318025+Vasavik-ua@users.noreply.github.com>
2023-09-17 20:07:03 +0100

    finishe standart request of hw

--- Hw.15.py
+++ Hw.15.py
@@ -1,38 +1,49 @@
-def exit_quest(value):  # Check input value for exit.
+def exit_quest(val):  # Check input value for exit.
     check_list = ["вихід", "exit", "quit", "e", "q"]
     for item in range(len(check_list)):
-        if check_list[item] == (value.lower()):
+        if check_list[item] == (val.lower()):
             return True
     return False
 
 
 def check_isdigit(value):
-    new_value = ()
-    check_minus = value.find('-')
-    check_point = value.find('.')
-    check_comma = value.find(',')
+    new_value = value
+    check_minus = new_value.find('-')
+    check_point = new_value.find('.')
+    check_comma = new_value.find(',')
     if check_minus >= 0:
-        new_value = value.replace('-', '', 1)
+        new_value = new_value.replace('-', '', 1)
     if check_point >= 0 or check_comma >= 0:
         if check_point >= 0:
-            new_value = value.replace('.', '', 1)
+            new_value = new_value.replace('.', '', 1)
         elif check_comma >= 0:
-            new_value = value.replace(',', '', 1)
+            new_value = new_value.replace(',', '', 1)
     if new_value.isdigit():
-        return new_value
+        if value[:1] == '-':
+            if check_point >= 0 or check_comma >= 0:
+                new_minus = (f'''Ви ввели від'ємне дробове число: {value}''')
+                return new_minus
+            else:
+                new_minint = (f'''Ви ввели від'ємне число: {value}''')
+                return new_minint
+        else:
+            if check_point >= 0 or check_comma >= 0:
+                new_intdb = (f'Ви ввели позитивне дробове число: {value}')
+                return new_intdb
+            else:
+                new_int = (f'Ви ввели позитивне ціле число: {value}')
+                return new_int
     else:
-        print(f'Ви ввели неправильне число: {value}')
         return False
-
 
 
 while True:
     input_value = input('Number required: ')
     if exit_quest(input_value):
         break
-    if check_isdigit(input_value) == False:
+    if not check_isdigit(input_value):
+        print(f'Ви ввели неправильне число: {input_value}')
         pass
     else:
+        print(check_isdigit(input_value))
         break
-
-
