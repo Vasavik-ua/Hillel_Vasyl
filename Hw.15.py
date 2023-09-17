8d9ad3f56ee291773f4bb9b5f816b55665bd891a
Vasavik-ua <47318025+Vasavik-ua@users.noreply.github.com>
2023-09-17 20:49:54 +0100

    improve positive target with '0'

--- Hw.15.py
+++ Hw.15.py
@@ -35,8 +35,12 @@ def check_isdigit(value):  # check the value and outcome the result.
                 return new_minint
         else:
             if check_point >= 0 or check_comma >= 0:
-                new_intdb = f'Ви ввели позитивне дробове число: {value}'
-                return new_intdb
+                if value[:1] == '.' or value[:1] == ',':
+                    new_intper = f'''Ви ввели позитивне дробове число: 0.{new_value}'''
+                    return  new_intper
+                else:
+                    new_intdb = f'Ви ввели позитивне дробове число: {value}'
+                    return new_intdb
             else:
                 new_int = f'Ви ввели позитивне ціле число: {value}'
                 return new_int
